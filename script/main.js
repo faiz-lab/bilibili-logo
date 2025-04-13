1b1d58161e89c46e25cb244a96c1daace446eb6f
faiz-lab <qyp555@gmail.com>
2025-04-13 19:06:02 +0900

    replace substring with slice

--- script/main.js
+++ script/main.js
@@ -140,8 +140,8 @@ const handler = () => {
         clearInterval(intervalId)
         return
     }
-    code.textContent = style_sheets.substring(7, n - 15)
-    demo.innerHTML = style_sheets.substring(0, n)
+    code.textContent = style_sheets.slice(7, n - 15)
+    demo.innerHTML = style_sheets.slice(0, n)
     codeblock.scrollTop = codeblock.scrollHeight
     hljs.highlightAll()
 }
